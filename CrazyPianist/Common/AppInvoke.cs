8cd8bf8519513935bc494cf6af912cae4ad21314
Jack Chen <jackqchen@outlook.com>
2025-03-26 14:42:30 +0800

    Update Win7 Version

--- CrazyPianist/Common/AppInvoke.cs
+++ CrazyPianist/Common/AppInvoke.cs
@@ -53,7 +53,6 @@ namespace CrazyPianist.Common
         public static IntPtr Init()
         {
             hWnd = FindWindow(null, "钢琴块2");
-            hWnd = FindWindowEx(hWnd, IntPtr.Zero, null, "");
             hWnd = FindWindowEx(hWnd, IntPtr.Zero, null, "Chrome Legacy Window");
             GetWindowRect(hWnd, out rect);
             Rectangle = new Rectangle(0, 0, rect.Right - rect.Left, rect.Bottom - rect.Top);
